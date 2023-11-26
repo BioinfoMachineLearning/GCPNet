f86156fb969406f51efde2bc2191910d1be4549e
Alex Morehead <acmwhb@missouri.edu>
2023-11-25 20:49:20 -0600

    Update test set run times script

--- scripts/measure_test_set_run_times.bash
+++ scripts/measure_test_set_run_times.bash
@@ -10,16 +10,13 @@ measure_time() {
 }
 
 # define checkpoint paths
-rs_model_ckpt_path="checkpoints/RS/model_1_epoch_54_accuracy_0_9873.ckpt"
 lba_model_ckpt_path="checkpoints/LBA/model_1_epoch_205_rmse_1_352_pearson_0_612_spearman_0_609.ckpt"
 psr_model_ckpt_path="checkpoints/PSR/model_epoch_115_localpearson_0_616_localspearman_0_532_localkendall_0_385_globalpearson_0_871_globalspearman_0_869_globalkendall_0_676.ckpt"
 nms_small_model_ckpt_path="checkpoints/NMS/NMS_Small/model_epoch_9977_mse_0_0070.ckpt"
 nms_small_20body_model_ckpt_path="checkpoints/NMS/NMS_Small_20Body/model_epoch_10087_mse_0_0071.ckpt"
 nms_static_model_ckpt_path="checkpoints/NMS/NMS_Static/model_epoch_5159_mse_0_0073.ckpt"
 nms_dynamic_model_ckpt_path="checkpoints/NMS/NMS_Dynamic/model_epoch_9825_mse_0_0173.ckpt"
-
-# measure time taken for for RS task
-measure_time "python3 src/eval.py datamodule=rs model=gcpnet_rs logger=csv trainer.accelerator=gpu trainer.devices=1 ckpt_path=\"$rs_model_ckpt_path\""
+rs_model_ckpt_path="checkpoints/RS/model_1_epoch_54_accuracy_0_9873.ckpt"
 
 # measure time taken for for LBA task
 measure_time "python3 src/eval.py datamodule=atom3d_lba model=gcpnet_lba logger=csv trainer.accelerator=gpu trainer.devices=1 ckpt_path=\"$lba_model_ckpt_path\""
@@ -32,3 +29,6 @@ measure_time "python3 src/eval.py datamodule=nms datamodule.data_mode=small mode
 measure_time "python3 src/eval.py datamodule=nms datamodule.data_mode=small_20body model=gcpnet_nms logger=csv trainer.accelerator=gpu trainer.devices=1 ckpt_path=\"$nms_small_20body_model_ckpt_path\""
 measure_time "python3 src/eval.py datamodule=nms datamodule.data_mode=static model=gcpnet_nms logger=csv trainer.accelerator=gpu trainer.devices=1 ckpt_path=\"$nms_static_model_ckpt_path\""
 measure_time "python3 src/eval.py datamodule=nms datamodule.data_mode=dynamic model=gcpnet_nms logger=csv trainer.accelerator=gpu trainer.devices=1 ckpt_path=\"$nms_dynamic_model_ckpt_path\""
+
+# measure time taken for for RS task
+measure_time "python3 src/eval.py datamodule=rs model=gcpnet_rs logger=csv trainer.accelerator=gpu trainer.devices=1 ckpt_path=\"$rs_model_ckpt_path\""
